6935a190dd1ce445ddbf8a0f25205677a060e992
fatihssahin <fatihssahinhome@gmail.com>
2026-08-24 14:47:19 +0300

    chore: initialize Spring Boot project

---
+++ src/main/java/com/todoflow/TodoFlowApplication.java
@@ -0,0 +1,13 @@
+package com.todoflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoFlowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/todoflow/TodoFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todoflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
